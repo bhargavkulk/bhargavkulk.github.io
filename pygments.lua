0055ee247c45f934bd29ae668d1957dbe7a2f251
Bhargav Kulkarni <bhargav.kulkarni1@gmail.com>
2024-09-13 03:54:53 -0600

    new blog post

--- pygments.lua
+++ pygments.lua
@@ -1,5 +1,10 @@
+local logging = require "logging"
+
 function CodeBlock(el)
     if el.classes[1] then
+        if el.classes[1] == "math" then
+            return pandoc.Div(el.text, { class = "math" })
+        end
         local result = pandoc.pipe("pygmentize", { "-f", "html", "-O", "style=bw", "-l", el.classes[1] }, el.text)
         if type(result) == "string" then
             return pandoc.RawBlock("html", result)
